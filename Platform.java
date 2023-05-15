0bf2bfbec98907f5ede5b8eeb2290e586c7a8b7a
Shmolol <karlalvarado666@gmail.com>
2023-05-15 15:44:56 -0400

    made the platform width not final so we can implement powerups later

--- Platform.java
+++ Platform.java
@@ -12,8 +12,8 @@ public class Platform extends Actor
      * Act - do whatever the Plateform wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public static final int WIDTH = 80;
-    public static final int HEIGHT = 10;
+    public static int WIDTH = 80;
+    public static int HEIGHT = 10;
     
     public void act() {
         if (Greenfoot.isKeyDown("left")) {
